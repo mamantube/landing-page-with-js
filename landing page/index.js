46707d0a8f0ac1eeae50b2111ba543e2d2951946
mamantube <firmanhiday.id007@gmail.com>
2023-01-19 22:50:13 +0700

    finishing

--- landing page/index.js
+++
@@ -1,9 +0,0 @@
-var big_wrapper;
-var toggle_btn;
-var hamburger_menu;
-
-function declare () {
-    toggle_btn = document.querySelector(".toggle-btn");
-    big_wrapper = document.querySelector(".big-wrapper");
-    hamburger_menu = document.querySelector(".hamburger-menu");
-}
