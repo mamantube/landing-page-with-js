107360af4cb6a79abf12e011a0ab68525c5c5996
mamantube <firmanhiday.id007@gmail.com>
2023-01-19 21:32:26 +0700

    css done

---
+++ landing page/index.js
@@ -0,0 +1,9 @@
+var big_wrapper;
+var toggle_btn;
+var hamburger_menu;
+
+function declare () {
+    toggle_btn = document.querySelector(".toggle-btn");
+    big_wrapper = document.querySelector(".big-wrapper");
+    hamburger_menu = document.querySelector(".hamburger-menu");
+}
